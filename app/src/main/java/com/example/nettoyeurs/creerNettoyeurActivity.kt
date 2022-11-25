336ab3c3e8d96c77d04119339b15df140cb6d4ac
bibui1994 <tranminhthang38@gmail.com>
2022-11-25 11:31:55 +0100

    25/11/2022 session 1

---
+++ app/src/main/java/com/example/nettoyeurs/creerNettoyeurActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nettoyeurs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class creerNettoyeurActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creer_nettoyeur)
+
+    }
+}
